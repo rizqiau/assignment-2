9b8839f0978cecd74b71192523fcd5ed5ff25612
Mochammad Rizqi Aullia <mcrizqi12@gmail.com>
2023-09-05 23:21:00 +0700

    menambahkan confirm

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,15 @@ const menuNav = document.querySelector(".menu-navigation");
 menuBar.addEventListener("click", function () {
   menuNav.classList.toggle("menu-active");
 });
+
+const submit = document.querySelector(".submit");
+
+submit.addEventListener("click", function () {
+  const masuk = confirm("Login Dulu");
+if (masuk) {
+  const name = prompt("Siapa nama anda?");
+  alert(`Hello manchunian ${name}`);
+} else {
+  alert("Bye Bye");
+}
+})
